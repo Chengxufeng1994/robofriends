64a4b0b64c3cefe1bb5037ce8b8ed998740ad487
Xu Feng Cheng <benny12628@gmail.com>
2021-06-05 00:23:27 +0800

    src/api: add Header: cache-control

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,13 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com/',
+  headers: {
+    headers: {
+      'Cache-Control': {
+        'max-age': 31536000,
+      },
+    },
+  },
 });
 
 export default instance;
